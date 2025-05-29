ace0c6cecae0216318f435d271e38a4bac52ff89
znecesito <zacknecesito@gmail.com>
2025-05-28 22:56:53 -0500

    Set up CI pipeline: backend test + frontend build

---
+++ backend/test_app.py
@@ -0,0 +1,11 @@
+import unittest
+from app import app
+
+class BasicTestCase(unittest.TestCase):
+    def test_home(self):
+        tester = app.test_client(self)
+        response = tester.get('/api')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
